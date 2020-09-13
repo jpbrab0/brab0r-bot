21bc46389fe7e6f4920039bf19d4a3dbb5e3c988
João Pedro <jpresende502@gmail.com>
2020-09-13 13:33:13 -0300

    Tentando aplicar um metodo do solid pra deixar o código foda mas msm assim meu código ficou uma bosta

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const tmi = require("tmi.js");
 const fs = require("fs");
-const settings = require("./opts.json");
+const dotenv = require('dotenv')
 // Definir opções de configuração
+
+dotenv.config()
 const opts = {
   identity: {
     username:'brab0bot',
@@ -13,16 +15,11 @@ const opts = {
 const client = new tmi.client(opts);
 
 client.connect();
-function commands(alvo, contexto, mensagem, bot) {
-  if (bot) {
-    return;
-  }
 
-  var contador = Number(0);
-  const commandName = mensagem;
+function ban(alvo, contexto, mensagem, bot){
+  const commandName = mensagem
   if (String(commandName).includes("!ban")) {
     let msgRandom = Math.floor(Math.random() * 5) + 1;
-    let probability = Math.floor(Math.random() * 3000) + 1;
     let alvoBan = String(commandName).split(" ");
     if (alvoBan[1] == undefined) {
       client.say(
@@ -60,49 +57,75 @@ function commands(alvo, contexto, mensagem, bot) {
             alvo,
             `${alvoBan[1]} foi banido por falar que java é bom Kappa`
           );
-        if (probability >= 250 && probability <= 500) {
-          client.say(alvo, `/timeout ${alvoBan[1]} 100`);
-          client.say(alvo, `${alvoBan[1]} foi banido LUL`);
-        } else if (probability >= 1500 && probability <= 2300) {
-          client.say(alvo, `/timeout ${user[1]} 100`);
-          client.say(alvo, `${user[1]} foi banido LUL`);
-        } else {
-          client.say(alvo, "ninguem foi banido! SeemsGood");
-        }
+        
       }
     }
-  } else if (String(commandName).includes("!garen")) {
-    client.say(alvo, "Garen nunca mais voltou na minha humilde live...");
-  } else if (String(commandName).includes("!meta")) {
-    client.say(
-      alvo,
-      "Minha meta para este mês é ter 10 subs para fazer uma live inteira usando o VIM!"
-    );
-  } else if (String(commandName).includes("!discord")) {
+}
+}
+function meta(alvo, contexto, mensagem, bot){
+  const commandName = mensagem
+  if (String(commandName).includes("!meta")) {
+      client.say(
+        alvo,
+        "Minha meta para este mês é ter 10 subs para fazer uma live inteira usando o VIM!"
+      );
+  }
+}
+function discord(alvo, contexto ,mensagem, bot){
+  const commandName = mensagem
+  if (String(commandName).includes("!discord")) {
     client.say(alvo, "> https://discord.gg/v6gX9dK <");
-  } else if (commandName == "!hoje") {
+  }
+}
+function hoje(alvo, contexto, mensagem, bot){
+  const commandName = mensagem
+  if (commandName == "!hoje") {
     fs.readFile("hoje.txt", "utf8", (err, data) => {
       client.say(alvo, data);
     });
-  } else if (commandName == "!comandos") {
+  }
+}
+function comandos(alvo, contexto, mensagem, bot){
+  const commandName = mensagem
+  if (commandName == "!comandos") {
     fs.readFile("comandos.txt", "utf8", (err, data) => {
       client.say(alvo, data);
     });
-  } else if (String(commandName).includes("!github")) {
+  }
+}
+function github(alvo, contexto, mensagem, bot){
+  const commandName = mensagem
+  if (String(commandName).includes("!github")) {
     var userGithub = String(commandName).split(" ")[1];
     if (userGithub == undefined) {
       client.say(alvo, "https://github.com/jpbrab0");
     } else {
       client.say(alvo, `https://github.com/${userGithub}`);
     }
-  } else if(String(commandName).includes("!4noobs")){
-      client.say(alvo,`Agora não tem mais desculpa de que não tem dinheiro para estudar programação! Conheça a He4rt devs e o 4noobs! > https://github.com/he4rt/4noobs `)
-  } else if (String(commandName).includes("salve")){
-      client.say(alvo, `Olá, seja bem-vindo(a) a live! pepeDD  ` )
   }
 }
+function fornoobs(alvo,contexto, mensagem, bot){
+  const commandName = mensagem
+  if(String(commandName).includes("!4noobs")){
+    client.say(alvo,`Agora não tem mais desculpa de que não tem dinheiro para estudar programação! Conheça a He4rt devs e o 4noobs! > https://github.com/he4rt/4noobs `)
+  } else if (String(commandName).includes("salve")){
+    client.say(alvo, `Olá, seja bem-vindo(a) a live! pepeDD  ` )
+  }
+}
+function commands(alvo, contexto, mensagem, bot) {
+  if (bot) {
+    return;
+  }  
+  ban(alvo, contexto, mensagem, bot)
+  meta(alvo ,contexto, mensagem, bot)
+  discord(alvo, contexto ,mensagem, bot)
+  hoje(alvo, contexto, mensagem, bot)
+  comandos(alvo, contexto, mensagem, bot)
+  github(alvo, contexto, mensagem, bot)
+  fornoobs(alvo,contexto, mensagem, bot)
+}
 function entrouNoChatDaTwitch(endereco, porta) {
-  console.log(`* Bot entrou no endereço ${endereco}:${porta}`);
+  console.log(`Bot entrou no endereço ${endereco}:${porta}`);
 }
 client.on("connected", entrouNoChatDaTwitch);
-client.on("message", commands);
+client.on("message", commands)
