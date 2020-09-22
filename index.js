03e74b9000c87926349e13bbda8fc5cf6ac79d08
João Pedro <jpresende502@gmail.com>
2020-09-22 17:52:47 -0300

    Desrefatorando o código pq sim ._.

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const tmi = require("tmi.js");
 const fs = require("fs");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 // Definir opções de configuração
 
-dotenv.config()
+dotenv.config();
 const opts = {
   identity: {
-    username:'brab0bot',
+    username: "brab0bot",
     password: process.env.TOKEN,
   },
-  channels: ['jpbrab0'],
+  channels: ["jpbrab0"],
 };
 // Cria um cliente tmi com  nossas opções
 const client = new tmi.client(opts);
 
 client.connect();
 
-function ban(alvo, contexto, mensagem, bot){
-  const commandName = mensagem
-  if (String(commandName).includes("!ban")) {
+function runCommands(alvo, contexto, mensagem, bot) {
+  if (bot) {
+    return;
+  }
+  // so
+  if (String(mensagem).includes("!so")) {
+    var pessoaDivulgada = String(mensagem).split(" ")[1];
+    client.say(alvo, `!sh-so ${pessoaDivulgada}`);
+  } else if (String(mensagem).includes("!ban")) {
     let msgRandom = Math.floor(Math.random() * 5) + 1;
-    let alvoBan = String(commandName).split(" ");
+    let alvoBan = String(mensagem).split(" ");
     if (alvoBan[1] == undefined) {
       client.say(
         alvo,
@@ -57,74 +63,43 @@ function ban(alvo, contexto, mensagem, bot){
             alvo,
             `${alvoBan[1]} foi banido por falar que java é bom Kappa`
           );
-        
       }
     }
-}
-}
-function meta(alvo, contexto, mensagem, bot){
-  const commandName = mensagem
-  if (String(commandName).includes("!meta")) {
-      client.say(
-        alvo,
-        "Minha meta para este mês é ter 10 subs para fazer uma live inteira usando o VIM!"
-      );
-  }
-}
-function discord(alvo, contexto ,mensagem, bot){
-  const commandName = mensagem
-  if (String(commandName).includes("!discord")) {
+  } else if (String(mensagem).includes("!garen")) {
+    client.say(alvo, "As vezes o garen aparece aq...");
+  } else if (String(mensagem).includes("!meta")) {
+    client.say(
+      alvo,
+      "Minha meta para este mês é ter 10 subs para fazer uma live inteira usando o VIM!"
+    );
+  } else if (String(mensagem).includes("!discord")) {
     client.say(alvo, "> https://discord.gg/v6gX9dK <");
-  }
-}
-function hoje(alvo, contexto, mensagem, bot){
-  const commandName = mensagem
-  if (commandName == "!hoje") {
+  } else if (mensagem == "!hoje") {
     fs.readFile("hoje.txt", "utf8", (err, data) => {
       client.say(alvo, data);
     });
-  }
-}
-function comandos(alvo, contexto, mensagem, bot){
-  const commandName = mensagem
-  if (commandName == "!comandos") {
+  } else if (mensagem == "!comandos") {
     fs.readFile("comandos.txt", "utf8", (err, data) => {
       client.say(alvo, data);
     });
   }
-}
-function github(alvo, contexto, mensagem, bot){
-  const commandName = mensagem
-  if (String(commandName).includes("!github")) {
-    var userGithub = String(commandName).split(" ")[1];
+  if (String(mensagem).includes("!github")) {
+    var userGithub = String(mensagem).split(" ")[1];
     if (userGithub == undefined) {
       client.say(alvo, "https://github.com/jpbrab0");
     } else {
       client.say(alvo, `https://github.com/${userGithub}`);
     }
+  } else if (String(mensagem).includes("!4noobs")) {
+    client.say(
+      alvo,
+      `Agora não tem mais desculpa de que não tem dinheiro para estudar programação! Conheça a He4rt devs e o 4noobs! > https://github.com/he4rt/4noobs `
+    );
+  } else if (String(mensagem).includes("salve")) {
+    client.say(alvo, `Olá, seja bem-vindo(a) a live! pepeDD  `);
   }
 }
-function fornoobs(alvo,contexto, mensagem, bot){
-  const commandName = mensagem
-  if(String(commandName).includes("!4noobs")){
-    client.say(alvo,`Agora não tem mais desculpa de que não tem dinheiro para estudar programação! Conheça a He4rt devs e o 4noobs! > https://github.com/he4rt/4noobs `)
-  } else if (String(commandName).includes("salve")){
-    client.say(alvo, `Olá, seja bem-vindo(a) a live! pepeDD  ` )
-  }
-}
-function runCommands(alvo, contexto, mensagem, bot) {
-  if (bot) {
-    return;
-  }  
-  ban(alvo, contexto, mensagem, bot)
-  meta(alvo ,contexto, mensagem, bot)
-  discord(alvo, contexto ,mensagem, bot)
-  hoje(alvo, contexto, mensagem, bot)
-  comandos(alvo, contexto, mensagem, bot)
-  github(alvo, contexto, mensagem, bot)
-  fornoobs(alvo,contexto, mensagem, bot)
-}
-client.on("connected", (endereco,porta) => {
-  console.log("O bot ta on!")
+client.on("connected", (endereco, porta) => {
+  console.log("O bot ta on!");
 });
-client.on("message", runCommands)
+client.on("message", runCommands);
