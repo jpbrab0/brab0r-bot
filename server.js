66033ea85afed64c2c0c91684beff958633e02da
João Pedro <jpresende502@gmail.com>
2020-10-16 14:30:30 -0300

    Adicionando novos eventos para o bot(subs,giftsubs,raids,etc)

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const express = require("express");
 const nunjucks = require("nunjucks");
 const routes  = require("./routes");
+const { use } = require("./routes");
 const server = express();
 server.use(express.static("public"));
 
@@ -19,6 +20,9 @@ nunjucks.configure("src/app/views", {
   express: server,
   autoescape: false,
   noCache: true,
+});
+server.listen(3333, () => {
+  return console.log("o server está on na porta 3333!");
 });
 // Definir opções de configuração
 const opts = {
@@ -40,13 +44,33 @@ fs.readdirSync(`${__dirname}/commands`)
       require(`./commands/${file}`).default(client, target, context, message);
     });
 });
-server.listen(3333, () => {
-  return console.log("o server está on na porta 3333!");
-});
 client.on("connected", (endereco, porta) => {
   console.log("O bot ta on!");
 });
-
+client.on("raided", (channel, username, viewers) => {
+  client.say("#jpbrab0", `PogChamp ${username} invadiu a live com ${viewers}!! WOW \o/ \o/ \o/ `)
+});
+client.on("part", (channel, username, self) => {
+  console.log(username + ' saiu da live...')
+});
+client.on("join", (channel, username, self) => {
+  console.log(username + ' entrou na live!')
+});
+client.on("cheer", (channel, userstate, message) => {
+  client.say("#jpbrab0", `PogChamp vlw pelos bits!`)
+});
+client.on("subscription", (channel, username, method, message, userstate) => {
+  client.say("#jpbrab0", `PogChamp ${username} deu sub! Muito obg por acreditar no meu conteúdo! `)
+});
+client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
+  client.say("#jpbrab0", `DENOnimus distribuiu ${numbOfSubs} no chat!`)
+});
+client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
+  client.say("#jpbrab0", `${recipient} distribuiu um sub para ${username}!`)
+});
+client.on("resub", (channel, username, months, message, userstate, methods) => {
+  client.say("#jpbrab0", `${username} deu resub por ${months}!`)
+});
 // configurando bot discord.
 const bot = new Discord.Client()
 
@@ -55,34 +79,3 @@ bot.login(process.env.DISCORD_TOKEN)
 bot.on("ready", () => {
   console.log("discord bot ta on")
 })
-// let prefix = "!"
-// let avatars = [
-//   'https://imagem.natelinha.uol.com.br/grande/faustaocomendo_86b41c9ca68c6778195f5f5d9dc659a2d54cd81a.jpeg',
-//   'https://contigo.uol.com.br/images/large/2020/09/26/danca-dos-famosos-clima-e-de-nervosismo-nos-bastidores-do-quadro-do-faustao-1268451.jpg',
-//   'https://static1.purepeople.com.br/articles/6/27/65/96/@/3142139-faustao-repetiu-uma-blusa-no-domingao-d-624x600-1.jpg',
-//   'https://static.ndonline.com.br/2020/02/fausto-17022020095816311.jpeg'
-// ]
-// let frases = [
-//   'OLOCO!',
-//   'TA PEGANDO FOGO BICHO!',
-//   'Essa fera ai, meu!',
-//   'Olha o tamanho da criança',
-//   'Voltamos já com vídeo cassetadas!'
-// ]
-// bot.on("message", (msg) => {
-//   let randomAvatar = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
-//   let randomFrase = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
-//   if(msg.content == `${prefix}faustão`){
-//     msg.channel.fetchWebhooks(webhook => {
-      
-//     })
-//     msg.channel.createWebhook('Faustão', {
-//       avatar:avatars[randomAvatar]
-//     }).then(webhook => {
-//       webhook.send(frases[randomFrase], {
-//         username:'Faustão',
-//         avatar:avatars[randomAvatar],
-//       })
-//     })
-//   }
-// })
